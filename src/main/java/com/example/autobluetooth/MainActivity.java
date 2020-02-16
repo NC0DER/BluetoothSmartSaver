491dc5eb4de97650f8c5f302e3cf38e382cafe21
Nikos <dynarithm@gmail.com>
2020-02-17 01:30:07 +0200

    Implement code to start and stop the service in Main
    + Remove unnecessary lines of code from Main.

--- src/main/java/com/example/autobluetooth/MainActivity.java
+++ src/main/java/com/example/autobluetooth/MainActivity.java
@@ -2,58 +2,79 @@ package com.example.autobluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothAdapter;
+import static com.example.autobluetooth.Utility.display;
+
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Switch;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.Switch;
-import android.widget.Toast;
+import android.bluetooth.BluetoothAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    Switch autoSwitch;
-    BluetoothAdapter bluetooth;
 
-    private void display(String text){
-        //Use Toast to display feedback to the user.
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    private Intent service_intent;
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i ("Service status", "Running");
+                return true;
+            }
+        }
+        Log.i ("Service status", "Not running");
+        return false;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final CheckCallStatusService service;
+        final Switch autoSwitch;
+        final BluetoothAdapter bluetooth;
+        final Context context;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         autoSwitch = findViewById(R.id.autoSwitch);
         bluetooth = BluetoothAdapter.getDefaultAdapter();
+        context = getApplicationContext();
+
+        // If the device doesn't support Bluetooth, then do an early clean exit.
+        //if (bluetooth == null) {
+        //  finishAndRemoveTask();
+        //}
 
         // set the switch to ON
         autoSwitch.setChecked(true);
         // attach a listener to check for changes in state
         autoSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    display("Enabling Bluetooth...");
-                    if (bluetooth == null) {
-                        display("Error: Device doesn't support Bluetooth");
-                    } else if (!bluetooth.isEnabled()) {
-                        // Bluetooth is disabled, enable it.
-                        bluetooth.enable();
-                        display("Bluetooth has been enabled!");
-                    }
-                } else {
-                    display("Disabling Bluetooth...");
-                    if (bluetooth == null) {
-                        display("Error: Device doesn't support Bluetooth");
-                    } else if (bluetooth.isEnabled()) {
-                        // Bluetooth is enabled, disable it.
-                        bluetooth.disable();
-                        display("Bluetooth has been disabled!");
+                    display("True", context);
+            } else {
+                if (bluetooth.isEnabled()) {
+                    // Bluetooth is enabled, disable it.
+                    bluetooth.disable();
                     }
                 }
             }
         });
 
+        service = new CheckCallStatusService();
+        service_intent = new Intent(this, service.getClass());
+        if (!isMyServiceRunning(service.getClass())) {
+            startService(service_intent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(service_intent);
+        super.onDestroy();
     }
 }
-

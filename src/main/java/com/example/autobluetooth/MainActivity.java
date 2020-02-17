63bd262ce55ef5f46a441c2c18af824f8f40fc67
Nikos <dynarithm@gmail.com>
2020-02-17 15:13:55 +0200

    Fix imports, Add JavaDoc comments, break long lines in MainActivity class

--- src/main/java/com/example/autobluetooth/MainActivity.java
+++ src/main/java/com/example/autobluetooth/MainActivity.java
@@ -18,76 +18,118 @@ package com.example.autobluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static com.example.autobluetooth.Utility.display;
-
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.content.Context;
-import android.util.Log;
 import android.widget.Switch;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.bluetooth.BluetoothAdapter;
 
+import static com.example.autobluetooth.Utility.display;
+import static com.example.autobluetooth.Utility.logI;
+/**
+ * Constructs the UI elements,
+ * and starts / stops the foreground
+ * service based on the switch value.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // Declare service_intent as a shared variable
+    // for onCreate() and onDestroy() methods.
     private Intent service_intent;
+    /**
+    * Checks where the service, is running.
+    * Returns True if it is, false otherwise.
+    * It also logs the service status,
+    * when the Debug Flag is set.
+    */
     private boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+        ActivityManager manager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service :
+                manager.getRunningServices(Integer.MAX_VALUE)) {
             if (serviceClass.getName().equals(service.service.getClassName())) {
-                Log.i ("Service status", "Running");
+                logI ("Service status", "Running");
                 return true;
             }
         }
-        Log.i ("Service status", "Not running");
+        logI ("Service status", "Not running");
         return false;
     }
 
+    /**
+     * Creates the UI elements.
+     * Implements the handler
+     * for the switch change.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         final CheckCallStatusService service;
         final Switch autoSwitch;
-        final BluetoothAdapter bluetooth;
+        //final BluetoothAdapter bluetooth;
         final Context context;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         autoSwitch = findViewById(R.id.autoSwitch);
-        bluetooth = BluetoothAdapter.getDefaultAdapter();
+        //bluetooth = BluetoothAdapter.getDefaultAdapter();
         context = getApplicationContext();
+        service = new CheckCallStatusService();
+        service_intent = new Intent(this, service.getClass());
 
-        // If the device doesn't support Bluetooth, then do an early clean exit.
+        // If the device doesn't support Bluetooth,
+        // then do an early clean exit.
         //if (bluetooth == null) {
         //  finishAndRemoveTask();
         //}
 
-        // set the switch to ON
-        autoSwitch.setChecked(true);
-        // attach a listener to check for changes in state
+        // If the service is running, set the switch ON.
+        // Else turn the switch OFF.
+        if (isMyServiceRunning(service.getClass())) {
+            autoSwitch.setChecked(true);
+        } else {
+            autoSwitch.setChecked(false);
+        }
+
         autoSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            /**
+             * Checks for changes in the state of the switch.
+             * The switch starts / stops the service, based on user interaction.
+             */
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    display("True", context);
-            } else {
-                if (bluetooth.isEnabled()) {
-                    // Bluetooth is enabled, disable it.
-                    bluetooth.disable();
+                    // If the switch is ON, start the service, if it is not running.
+                    if (!isMyServiceRunning(service.getClass())) {
+                        display("Starting Service...", context);
+                        startService(service_intent);
+                        display("Service has been started.", context);
+                    }
+                } else {
+                    // If the switch is OFF, stop the service, if it is running.
+                    if (isMyServiceRunning(service.getClass())) {
+                        display("Stopping Service...", context);
+                        stopService(service_intent);
+                        display("Service has been stopped.", context);
                     }
                 }
             }
         });
 
-        service = new CheckCallStatusService();
-        service_intent = new Intent(this, service.getClass());
-        if (!isMyServiceRunning(service.getClass())) {
-            startService(service_intent);
-        }
-    }
+        //if (bluetooth.isEnabled()) {
+        // Bluetooth is enabled, disable it.
+        //bluetooth.disable();
+        //}
 
+    }
+    /**
+     * Stops the service at app exit,
+     * then calls the onDestroy()
+     * method of the parent class.
+     */
     @Override
     protected void onDestroy() {
         stopService(service_intent);

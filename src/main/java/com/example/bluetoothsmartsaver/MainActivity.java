e9b70a8610d1c5a9054be764bbf0798ab7d095f2
Nikos <dynarithm@gmail.com>
2020-02-25 20:07:18 +0200

    Add app icon for most common screen dimensions
    + Refactor some code, resources and comments.

---
+++ src/main/java/com/example/bluetoothsmartsaver/MainActivity.java
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2020 Nikolaos Giarelis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bluetoothsmartsaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.TextView;
+
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
+import static android.view.View.TEXT_ALIGNMENT_TEXT_START;
+import static com.example.bluetoothsmartsaver.Utility.createStatusSpannable;
+import static com.example.bluetoothsmartsaver.Utility.display;
+import static com.example.bluetoothsmartsaver.Utility.getActionbarHeight;
+import static com.example.bluetoothsmartsaver.Utility.logI;
+
+/**
+ * Constructs the UI elements,
+ * and starts / stops the foreground
+ * service based on the switch value.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Declare service_intent as a shared variable
+    // for onCreate() and onDestroy() methods.
+    private Intent service_intent;
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+    private ConstraintLayout parentLayout;
+    private TextView popupTitle;
+    private TextView popupText;
+    private int parentSize;
+    private int actionBarHeight;
+
+    /**
+     * Checks where the service, is running.
+     * Returns True if it is, false otherwise.
+     * It also logs the service status,
+     * when the Debug Flag is set.
+     */
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service :
+                manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                logI ("Service status", "Running");
+                return true;
+            }
+        }
+        logI ("Service status", "Not running");
+        return false;
+    }
+
+    /**
+     * Creates the UI elements.
+     * Implements the handler
+     * for the switch change.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final HandleBluetoothByCallStateStatus service =
+                new HandleBluetoothByCallStateStatus();
+        final BluetoothAdapter bluetooth =
+                BluetoothAdapter.getDefaultAdapter();
+        final Context context =
+                getApplicationContext();
+        // Need to be assigned after the setContentView call.
+        final Switch autoSwitch;
+        final TextView help;
+        final TextView status;
+
+
+        // Create, set content view and retrieve its elements.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        autoSwitch = findViewById(R.id.autoSwitch);
+        help = findViewById(R.id.HelpTextView);
+        status = findViewById(R.id.ServiceTextView);
+        parentLayout = findViewById(R.id.layout);
+
+        help.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Displays a popup, when the help TextView is pressed.
+             * The popup can be dismissed when the user clicks elsewhere.
+             */
+            @Override
+            public void onClick(View view) {
+                // Initialize a new instance of LayoutInflater service.
+                layoutInflater = (LayoutInflater)
+                        context.getSystemService(LAYOUT_INFLATER_SERVICE);
+                // Inflate the custom view.
+                ViewGroup customView = (ViewGroup)
+                        layoutInflater.inflate(R.layout.popup_window, null);
+
+                // Get ActionBarHeight.
+                actionBarHeight = getActionbarHeight(context);
+                // Find Popup Title and text.
+                popupTitle = customView.findViewById(R.id.PopupTitleView);
+                popupText = customView.findViewById(R.id.PopupTextView);
+
+                // Set text alignment of popup text.
+                // For build version greater or equal to Android Oreo (API Level 26)
+                // Full text justification is supported, and will also be used below.
+                // For older builds, left align justification is used.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    popupText.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+                } else {
+                    popupText.setTextAlignment(TEXT_ALIGNMENT_TEXT_START);
+                }
+                // Dynamically set padding of Popup title,
+                // based to a fraction of the action bar height.
+                popupTitle.setPadding(
+                        0,
+                        actionBarHeight / 3,
+                        0,
+                        actionBarHeight / 3);
+                // Get view sizes from parent.
+                parentSize = ViewPager.LayoutParams.MATCH_PARENT;
+                // Create a focusable PopupWindow.
+                popupWindow = new PopupWindow(
+                        customView,
+                        parentSize,
+                        parentSize,
+                        true);
+                popupWindow.showAtLocation(parentLayout, Gravity.CENTER, 500, 500);
+                popupWindow.setBackgroundDrawable(
+                        new BitmapDrawable(context.getResources(), (Bitmap) null));
+                customView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+            }
+        });
+
+        // If the device doesn't support Bluetooth,
+        // then do an early clean exit.
+        if (bluetooth == null) {
+            finishAndRemoveTask();
+        }
+
+        service_intent = new Intent(this, service.getClass());
+
+        // If the service is running, set the switch ON.
+        // Else turn the switch OFF.
+        // Also update the service status TextView.
+        if (isMyServiceRunning(service.getClass())) {
+            autoSwitch.setChecked(true);
+            status.setText(
+                    createStatusSpannable("Service Status: ", true),
+                    TextView.BufferType.SPANNABLE);
+        } else {
+            autoSwitch.setChecked(false);
+            status.setText(
+                    createStatusSpannable("Service Status: ", false),
+                    TextView.BufferType.SPANNABLE);
+        }
+        // Make TextView text bold, in both cases.
+        status.setTypeface(status.getTypeface(), Typeface.BOLD);
+
+        autoSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            /**
+             * Checks for changes in the state of the switch.
+             * The switch starts / stops the service, based on user interaction.
+             */
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // If the switch is ON, try to start the service,
+                    // if it is not running, and set "Service status:
+                    // Started" in the service TextView, if successful.
+                    if (!isMyServiceRunning(service.getClass())) {
+                        try{
+                            startService(service_intent);
+                        } catch (SecurityException sec) {
+                            display("Error: Service fail to run. " +
+                                    "Please allow requested app permissions.", context);
+                        } catch (IllegalStateException ise) {
+                            display("Error: Service couldn't be started.", context);
+                        }
+                        status.setText(
+                                createStatusSpannable("Service Status: ", true),
+                                TextView.BufferType.SPANNABLE);
+                    }
+                } else {
+                    // If the switch is OFF, try to stop the service,
+                    // if it is running, and set "Service status:
+                    // Stopped" in the service TextView, if successful.
+                    if (isMyServiceRunning(service.getClass())) {
+                        try{
+                            stopService(service_intent);
+                        } catch (SecurityException sec) {
+                            display("Error: Service fail to run. " +
+                                    "Please allow requested app permissions.", context);
+                        } catch (IllegalStateException ise) {
+                            display("Error: Service couldn't be stopped.", context);
+                        }
+                        status.setText(
+                                createStatusSpannable("Service Status: ", false),
+                                TextView.BufferType.SPANNABLE);
+                    }
+                }
+                // Make TextView text bold, in both cases.
+                status.setTypeface(status.getTypeface(), Typeface.BOLD);
+            }
+        });
+    }
+    /**
+     * Calls the onDestroy()
+     * method of the parent class.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ src/main/java/com/example/bluetoothsmartsaver/Utility.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 Nikolaos Giarelis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bluetoothsmartsaver;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.Toast;
+
+/**
+ * Contains static utility methods,
+ * that are required by other classes
+ * of the app.
+ * */
+@SuppressWarnings("WeakerAccess")
+public final class Utility {
+    // Set logging, depending on the value of the debug flag.
+    static final boolean LOG = BuildConfig.DEBUG;
+    /** Uses Toast to display feedback to the user for a short length of time. */
+    protected static void display(String text, Context context) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+
+    /** If logging is enabled, then we print the log info messages.
+     *  Otherwise, the compiler optimizes away the empty method.
+     */
+    protected static void logI(String tag, String msg){ if(LOG) Log.i(tag, msg); }
+
+    /** Dynamically get the height of the Actionbar for scaling other UI elements. */
+    protected static int getActionbarHeight(Context context){
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (context.getTheme().resolveAttribute(
+                android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(
+                    tv.data, context.getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
+    }
+
+    /** Create a Service Status string based on specific inputs. */
+    protected static Spannable createStatusSpannable(String text, Boolean started) {
+        String status;
+        int color;
+        if (started) {
+            status = "Started";
+            color = Color.parseColor("#40ff40");
+        } else {
+            status = "Stopped";
+            color = Color.parseColor("#ff1111");
+        }
+        String total = text + status;
+        Spannable spannable = new SpannableString(total);
+        spannable.setSpan(
+                new ForegroundColorSpan(color),
+                text.length(), total.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannable;
+    }
+}
